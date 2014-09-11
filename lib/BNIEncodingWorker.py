20431a6eb49707252d54aca8bc510b17ae10a6d1
Jacob Sanford <jsanford@unb.ca>
2014-09-11 13:11:22 -0300

    Add various integrity checks before we consider files processed

--- lib/BNIEncodingWorker.py
+++ lib/BNIEncodingWorker.py
@@ -13,7 +13,8 @@ import threading
 class BNIEncodingWorker(threading.Thread):
     def __init__(self, worker_id, config, logger, queue):
         threading.Thread.__init__(self)
-        self.cur_file = ''
+        self.cur_tif = ''
+        self.cur_jpg = ''
         self.basename = ''
         self.worker_id = worker_id
         self.init_config(config)
@@ -29,29 +30,32 @@ class BNIEncodingWorker(threading.Thread):
             self.logger.info('Worker %s does not have a task assigned. Looking for one.', self.worker_id)
             self.logger.info('Worker %s reports queue length is currently %s.', self.worker_id, len(self.queue))
             try:
-                self.cur_file = self.queue.pop()
+                self.setup_next_image()
                 self.process_file()
             except:
                 break
 
     def process_file(self):
-        if (self.generate_basename() and
+        if (self.check_tif_size() and
+            self.check_jpg_exits() and
+            self.check_jpg_size() and
             self.generate_hocr() and
             self.generate_ocr() and
-            self.generate_sha1()
-        ):
-            self.mv_tiff_out()
+            self.cp_tiff_out() and
             self.cp_jpg_out()
-            self.send_jpg_glacier()
+        ):
+            pass
+            # self.remove_originals()
+            # self.generate_sha1() MOVE THIS TO SELF-AWARE COPY / MOVE FUNCTIONS
 
     def generate_hocr(self):
-        self.logger.info('Worker %s generating OCR for %s.', self.worker_id, self.cur_file)
+        self.logger.info('Worker %s generating OCR for %s.', self.worker_id, self.cur_tif)
         surrogate_output_filepath = '.'.join( (self.basename, 'tiff'))
 
         gm_call = [
                   self.config.get('GraphicsMagick', 'gm_bin_path'),
                   "convert",
-                  self.cur_file
+                  self.cur_tif
                   ]
         self.append_additional_encode_options(gm_call, 'gm_surrogate_convert_options', 'GraphicsMagick')
         gm_call.append(surrogate_output_filepath)
@@ -83,13 +87,10 @@ class BNIEncodingWorker(threading.Thread):
         self.logger = logger
         self.logger.info('Worker %s appears!', self.worker_id)
 
-    def mv_tiff_out(self):
+    def cp_tiff_out(self):
         pass
 
     def cp_jpg_out(self):
-        pass
-
-    def send_jpg_glacier(self):
         pass
 
     def generate_sha1(self):
@@ -97,11 +98,11 @@ class BNIEncodingWorker(threading.Thread):
         sha1sum_filep = open(sha1sum_filename, "w")
         sha1sum_call = [
             '/usr/bin/sha1sum',
-            os.path.basename(self.cur_file),
+            os.path.basename(self.cur_tif),
             '.'.join((os.path.basename(self.basename), 'jpg')),
             '.'.join((os.path.basename(self.basename), 'hocr')),
         ]
-        if subprocess.call(sha1sum_call, stdout=sha1sum_filep, cwd=os.path.dirname(self.cur_file)) == 0:
+        if subprocess.call(sha1sum_call, stdout=sha1sum_filep, cwd=os.path.dirname(self.cur_tif)) == 0:
             self.logger.info('Worker %s succeded in calculating SHA1sum of original file %s.', self.worker_id, sha1sum_filename)
             return True
         self.logger.info('Worker %s failed in calculating SHA1sum of original file %s.', self.worker_id, sha1sum_filename)
@@ -117,7 +118,7 @@ class BNIEncodingWorker(threading.Thread):
         return True
 
     def generate_basename(self):
-        self.basename = self.cur_file[0:self.cur_file.rindex('.')]
+        self.basename = self.cur_tif[0:self.cur_tif.rindex('.')]
         return True
 
     def append_additional_encode_options(self, call_list, extra_options_variable, encoder_name):
@@ -130,13 +131,13 @@ class BNIEncodingWorker(threading.Thread):
         return re.split("[ ,]+", options_string)
 
     def log_encode_begin(self):
-        self.logger.info('Worker %s encoding %s surrogate.', self.worker_id, self.cur_file)
+        self.logger.info('Worker %s encoding %s surrogate.', self.worker_id, self.cur_tif)
 
     def log_encode_fail(self):
-        self.logger.error('Worker %s encoding surrogate of %s has failed.', self.worker_id, self.cur_file)
+        self.logger.error('Worker %s encoding surrogate of %s has failed.', self.worker_id, self.cur_tif)
 
     def log_encode_success(self):
-        self.logger.info('Worker %s encoding surrogate of %s has succeeded.', self.worker_id,self.cur_file)
+        self.logger.info('Worker %s encoding surrogate of %s has succeeded.', self.worker_id,self.cur_tif)
 
     def distill_hocr_to_ocr(self, hocr_string):
         ocr_string=''
@@ -148,3 +149,26 @@ class BNIEncodingWorker(threading.Thread):
                 ).encode('utf-8').split()
             ) + "\n"
         return ocr_string
+
+    def check_tif_size(self):
+        return self.check_file_size(self.cur_tif, int(self.config.get('MinimumSizes', 'min_size_tif')))
+
+    def check_jpg_exits(self):
+        return os.path.isfile(self.cur_jpg)
+
+    def check_jpg_size(self):
+        return self.check_file_size(self.cur_jpg, int(self.config.get('MinimumSizes', 'min_size_jpg')))
+
+    def check_file_size(self, file_path, minimum_size):
+        if os.path.getsize(file_path) > minimum_size:
+            return True
+        return False
+
+    def setup_next_image(self):
+        self.cur_tif = self.queue.pop()
+        self.generate_basename()
+        self.cur_jpg = os.path.normpath(
+            os.path.dirname(self.cur_tif) +
+            self.config.get('Locations', 'relative_location_jpg') +
+            '.'.join((os.path.basename(self.basename), 'jpg'))
+        )
